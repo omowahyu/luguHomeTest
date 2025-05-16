9b8b44fa5cd5b39b19a35b1bf40446935d4f527e
omowahyu <omowahyu24@gmail.com>
2025-05-16 12:56:19 +0700

    improve headers title responsive

--- components/Templates/BasicLayout.tsx
+++ components/Templates/BasicLayout.tsx
@@ -18,7 +18,9 @@ export default function BasicLayout({ children }: { children: ReactNode }) {
       <main className=" p-4 w-full max-w-5xl mx-auto mt-40">
         <Toaster />
         <div className="flex justify-between  items-center mb-6">
-          <h1 className="text-4xl font-medium line-">Platzi API Playground</h1>
+          <h1 className="hidden md:block md:text-4xl font-medium line-">
+            Platzi API Playground
+          </h1>
           <Button
             className="bg-[#D1EE6E] text-black hover:text-white"
             onClick={() => onOpen()}
